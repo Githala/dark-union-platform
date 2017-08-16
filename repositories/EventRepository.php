4e5d37c2dd83971dbeddb6728f382d5043ab85b2
Arne-Cloudwise <arne@cloudwise.nl>
2017-08-16 17:16:20 +0200

    Use DB to fetch data.

--- repositories/EventRepository.php
+++ repositories/EventRepository.php
@@ -9,22 +9,51 @@
 
 include_once plugin_dir_path( __FILE__ )."../models/Event.php";
 
+
+
 class EventRepository {
 
-	private $events;
+	private $db;
+	private $table_name;
 
-	function __construct() {
-		$this->events = array(
-			1 => new Event(1,"Live 1", 6),
-			2 => new Event(2,"Baravond 1", 3)
-		);
+	public function __construct() {
+		global $wpdb;
+		$this->db = $wpdb;
+		$this->table_name = $wpdb->prefix . "events";
 	}
 
+
 	function getEventById($id) {
-		return $this->events[$id];
+		$query = $this->db->prepare(
+			"
+					SELECT * FROM $this->table_name
+					WHERE id = %d
+				",
+			$id
+		);
+
+		$row = $this->db->get_row( $query);
+		return new Event($row->id, $row->name, $row->ep);
 	}
 
 	function getAllEvents() {
 		return $this->events;
+	}
+
+	function create_table() {
+		global $wpdb;
+
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $this->table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        name varchar(50) NOT NULL,
+        ep smallint(5) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 	}
 }
